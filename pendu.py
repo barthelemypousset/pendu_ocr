4d6b0e2326e2c5f7a5810b6cfc699a12d576ba4f
barthelemy POUSSET <barthelemy.pousset@cleverconnect.com>
2019-11-13 17:22:47 +0100

    get back to work

--- pendu.py
+++ pendu.py
@@ -1,8 +1,29 @@
 import data
+
 import functions
 from random import randrange
 
-randWord = randrange(10)
+#pseudo = input("Whats your name ? ")
 
-pseudo = input("Whats your name ? ")
+randWord = data.words[randrange(10)]
 
+randWordSplit = list(randWord)
+randWordToGuess = "_"*len(randWordSplit)
+
+
+print("debug:")
+print(randWordSplit)
+print("===")
+
+print("")
+print("here is the word to guess:")
+print("".join(randWordToGuess))
+
+guessLetter = input("give me a letter")
+
+if guessLetter in randWordSplit:
+    letterFound = [randWordSplit.index(letter,i) for i,letter in enumerate(randWordSplit) if letter is guessLetter]
+    print("yeah you found {} letter".format(len(letterFound)))
+
+else:
+    print("This letter isn't in the word to guess")
